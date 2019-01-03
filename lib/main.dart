f25883c99de18e73fd121974892996b7315f8cc0
akshaygupta1996 <gupta.akshay1996@gmail.com>
2019-01-03 22:28:44 +0530

    2.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
 
 
 void main() => runApp(new MyApp());
@@ -21,8 +22,28 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-   return new Container();
+   return new Scaffold(
+     body: Stack(
+       children: <Widget>[
+         new Container(
+           decoration: new BoxDecoration(
+             image: new DecorationImage(
+               image: new ExactAssetImage('assets/book.jpg'),
+               fit: BoxFit.cover,
+             ),
+           ),
+           child: new BackdropFilter(
+             filter: new ui.ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
 
+               child: new Container(
+               decoration:
+               new BoxDecoration(color: Colors.white.withOpacity(0.0)),
+             ),
+           ),
+         ),
+       ],
+     ),
+   );
   }
 }
 
