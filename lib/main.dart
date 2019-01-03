2797566d967f8a249935be7330e43688754ff350
akshaygupta1996 <gupta.akshay1996@gmail.com>
2019-01-04 00:38:03 +0530

    3.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:ui' as ui;
 
-
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
@@ -22,28 +21,46 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-   return new Scaffold(
-     body: Stack(
-       children: <Widget>[
-         new Container(
-           decoration: new BoxDecoration(
-             image: new DecorationImage(
-               image: new ExactAssetImage('assets/book.jpg'),
-               fit: BoxFit.cover,
-             ),
-           ),
-           child: new BackdropFilter(
-             filter: new ui.ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
-
-               child: new Container(
-               decoration:
-               new BoxDecoration(color: Colors.white.withOpacity(0.0)),
-             ),
-           ),
-         ),
-       ],
-     ),
-   );
+    return new Scaffold(
+      body: Stack(
+        children: <Widget>[
+          new Container(
+            decoration: new BoxDecoration(
+              image: new DecorationImage(
+                image: new ExactAssetImage('assets/book.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: new BackdropFilter(
+              filter: new ui.ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
+              child: new Container(
+                decoration:
+                    new BoxDecoration(color: Colors.white.withOpacity(0.0)),
+              ),
+            ),
+          ),
+          new Column(
+            children: <Widget>[
+              new Flexible(flex: 6, child: new Container()),
+              new Flexible(
+                  flex: 5,
+                  child: new Container(
+                    color: Colors.black54,
+                  )),
+              new Flexible(
+                  flex: 2,
+                  child: new Container(
+                    padding: new EdgeInsets.only(top: 36.0),
+                    child: new Text(
+                      "SIGN UP",
+                      style:
+                          new TextStyle(color: Colors.white54, fontSize: 16.0),
+                    ),
+                  ))
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
-
